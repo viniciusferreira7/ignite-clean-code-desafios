41cbe916b800225439e5afcb407652a9b483efdd
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-09-23 10:51:10 -0300

    refactor: magic numbers

--- desafios/07-numeros-magicos.js
+++
@@ -1,5 +0,0 @@
-function lookForUpdades() {}
-
-setInterval(lookForUpdades, 60 * 30 * 1000)
-
-function calculateDiscount(price, discount) {}
---
+++ desafios/07-numeros-magicos.ts
@@ -0,0 +1,7 @@
+function lookForUpdates() {}
+
+const INTERVAL_30_MINUTES = 1000 * 60 * 30
+
+setInterval(lookForUpdates, INTERVAL_30_MINUTES)
+
+function calculateDiscount(price: number, discount: number) {}
